e844484939a39ef68f7729af7a31d8f2c030c2fa
eobcre <eobcre@gmail.com>
2023-01-11 17:16:32 -0500

    logic add tasks

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const taskForm = document.querySelector('#task-form');
-const taskInput = document.querySelector('#task-input');
-const taskSubmit = document.querySelector('#task-submit');
-const taskList = document.querySelector('#task-list');
+window.addEventListener('load', () => {
+  const taskForm = document.querySelector('#task-form');
+  const taskInput = document.querySelector('#task-input');
+  const taskSubmit = document.querySelector('#task-submit');
+  const taskList = document.querySelector('#task-list');
 
-// Stop refreshing the page
-taskForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+  const buttonAdd = (task) => {
+    const listItem = document.createElement('div');
+    const showItem = taskList.appendChild(listItem);
+    showItem.innerHTML = task;
+  };
+
+  // Add button
+  taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const task = taskInput.value;
+    buttonAdd(task);
+    taskInput.value = '';
+
+    if (!task) {
+      alert('Please add the task.');
+      return;
+    }
+  });
 });
