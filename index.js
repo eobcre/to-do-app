a9fe9250608fad59eb099b16b9a70f3dcb0adae3
eobcre <eobcre@gmail.com>
2023-01-11 22:44:02 -0500

    logic created delete button

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
 window.addEventListener('load', () => {
   const taskForm = document.querySelector('#task-form');
   const taskInput = document.querySelector('#task-input');
-  const taskSubmit = document.querySelector('#task-submit');
   const taskList = document.querySelector('#task-list');
 
-  const buttonAdd = (task) => {
-    const listItem = document.createElement('div');
-    const showItem = taskList.appendChild(listItem);
-    showItem.innerHTML = task;
-  };
-
-  // Add button
   taskForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const task = taskInput.value;
-    buttonAdd(task);
-    taskInput.value = '';
 
     if (!task) {
       alert('Please add the task.');
       return;
     }
+
+    addTask(task);
+    taskInput.value = '';
   });
+
+  // Add button
+  const addTask = (task) => {
+    const divItem = document.createElement('div');
+    const showItem = taskList.appendChild(divItem);
+    showItem.innerHTML = task;
+
+    // Delete button
+    const buttonDelete = document.createElement('button');
+    buttonDelete.innerHTML = 'Delete';
+    divItem.appendChild(buttonDelete);
+
+    buttonDelete.addEventListener('click', (e) => {
+      e.preventDefault();
+      deleteTask(buttonDelete);
+    });
+  };
+
+  const deleteTask = (buttonDelete) => {
+    const selectTask = buttonDelete.closest('div');
+    taskList.removeChild(selectTask);
+  };
 });
